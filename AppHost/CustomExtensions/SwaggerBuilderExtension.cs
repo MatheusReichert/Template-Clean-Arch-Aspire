4bff3abb9fd8b3ad97d2ca826626b7a73105057e
Matheus Ernan Reichert <matheus_ernan@outlook.com>
2025-04-20 13:58:02 -0300

    checkpoint

--- AppHost/CustomExtensions/SwaggerBuilderExtension.cs
+++ AppHost/CustomExtensions/SwaggerBuilderExtension.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System.Diagnostics;
 
 namespace AppHost.CustomExtensions;
@@ -24,7 +27,9 @@ internal static class SwaggerBuilderExtension
             },
             commandOptions: new CommandOptions()
             {
-                IconName = "LinkMultiple", IsHighlighted = true, IconVariant = IconVariant.Regular
+                IconName = "LinkMultiple",
+                IsHighlighted = true,
+                IconVariant = IconVariant.Regular
             }
         );
     }
--- ServiceDefaults/ServiceDefaultsCustomExtensions.cs
+++ ServiceDefaults/ServiceDefaultsCustomExtensions.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,8 +13,7 @@ using OpenTelemetry.Trace;
 
 namespace ServiceDefaults;
 
-
-public static class CustomExtensions
+public static class ServiceDefaultsCustomExtensions
 {
     public static TBuilder AddServiceDefaults<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
     {
